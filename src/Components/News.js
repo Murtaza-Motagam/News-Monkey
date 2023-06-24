95a75c099ea370cab3ede1beb796ec98aeb3b5e0
Murtaza Motagam <108859240+Murtaza-Motagam@users.noreply.github.com>
2023-06-24 13:57:06 +0530

    Update News.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -99,7 +99,7 @@ export class News extends Component {
 
   fetchMoreData = async () => {
     this.setState({ page: this.state.page + 1 });
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=cc1cbcf82bb94ce0a26c313d6e273977&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=XXXXXXXXXXXXXXXXXXX&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
